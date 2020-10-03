19ccceec471c9954be3f4c7b8230b970eb45365e
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-03 12:48:26 +0330

    implementing routing

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import About from './components/About';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
 import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import Service from './components/Service';
+import Single from './components/Single';
+import Team from './components/Team';
 
 const App = () => {
-  return (<Home />);
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact="true" />
+        <Route path="/about" component={About} exact="true" />
+        <Route path="/blog" component={Blog} exact="true" />
+        <Route path="/contact" component={Contact} exact="true" />
+        <Route path="/portfolio" component={Portfolio} exact="true" />
+        <Route path="/service" component={Service} exact="true" />
+        <Route path="/single" component={Single} exact="true" />
+        <Route path="/team" component={Team} exact="true" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
