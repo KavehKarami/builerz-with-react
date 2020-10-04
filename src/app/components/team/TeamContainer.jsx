c2e7faa97c7333210b0cd31ca0fb00c5a471b17f
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-04 13:11:15 +0330

    clean Team Page Component

--- src/app/components/team/TeamContainer.jsx
+++ src/app/components/team/TeamContainer.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import TeamItem from './TeamItem';
 
-const TeamContainer = () => {
+const TeamContainer = (props) => {
   return (
     <div className="row">
-      <TeamItem imgURL={require("../../../assets/img/team-1.jpg")} dataWowDelay="0.1s" person="Adam Phillips" responsibility="CEO & Founder" />
-      <TeamItem imgURL={require("../../../assets/img/team-2.jpg")} dataWowDelay="0.2s" person="Dylan Adams" responsibility="Civil Engineer" />
-      <TeamItem imgURL={require("../../../assets/img/team-3.jpg")} dataWowDelay="0.3s" person="Jhon Doe" responsibility="Interior Designer" />
-      <TeamItem imgURL={require("../../../assets/img/team-4.jpg")} dataWowDelay="0.4s" person="Josh Dunn" responsibility="Painter" />
-
-      <TeamItem imgURL={require("../../../assets/img/team-1.jpg")} dataWowDelay="0.1s" person="Adam Phillips" responsibility="CEO & Founder" />
-      <TeamItem imgURL={require("../../../assets/img/team-2.jpg")} dataWowDelay="0.2s" person="Dylan Adams" responsibility="Civil Engineer" />
-      <TeamItem imgURL={require("../../../assets/img/team-3.jpg")} dataWowDelay="0.3s" person="Jhon Doe" responsibility="Interior Designer" />
-      <TeamItem imgURL={require("../../../assets/img/team-4.jpg")} dataWowDelay="0.4s" person="Josh Dunn" responsibility="Painter" />
+      {props.children}
     </div>
   );
 }
--- src/app/components/team/TeamPage.jsx
+++ src/app/components/team/TeamPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TeamContainer from './TeamContainer';
+import TeamItem from './TeamItem';
 
 const TeamPage = () => {
   return (
@@ -10,7 +11,17 @@ const TeamPage = () => {
           <p>Our Team</p>
           <h2>Meet Our Engineer</h2>
         </div>
-        <TeamContainer />
+        <TeamContainer >
+          <TeamItem imgURL={require("../../../assets/img/team-1.jpg")} dataWowDelay="0.1s" person="Adam Phillips" responsibility="CEO & Founder" />
+          <TeamItem imgURL={require("../../../assets/img/team-2.jpg")} dataWowDelay="0.2s" person="Dylan Adams" responsibility="Civil Engineer" />
+          <TeamItem imgURL={require("../../../assets/img/team-3.jpg")} dataWowDelay="0.3s" person="Jhon Doe" responsibility="Interior Designer" />
+          <TeamItem imgURL={require("../../../assets/img/team-4.jpg")} dataWowDelay="0.4s" person="Josh Dunn" responsibility="Painter" />
+
+          <TeamItem imgURL={require("../../../assets/img/team-1.jpg")} dataWowDelay="0.1s" person="Adam Phillips" responsibility="CEO & Founder" />
+          <TeamItem imgURL={require("../../../assets/img/team-2.jpg")} dataWowDelay="0.2s" person="Dylan Adams" responsibility="Civil Engineer" />
+          <TeamItem imgURL={require("../../../assets/img/team-3.jpg")} dataWowDelay="0.3s" person="Jhon Doe" responsibility="Interior Designer" />
+          <TeamItem imgURL={require("../../../assets/img/team-4.jpg")} dataWowDelay="0.4s" person="Josh Dunn" responsibility="Painter" />
+        </TeamContainer>
       </div>
     </div>
   );
