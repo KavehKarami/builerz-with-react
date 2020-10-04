485bdfd44334e25409024a87183a87b0f82b3ae6
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-04 16:05:42 +0330

    wip-refactor Home Component

--- src/app/components/team/TeamPage.jsx
+++ src/app/components/team/TeamPage.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import TeamContainer from './TeamContainer';
-import TeamItem from './TeamItem';
 
-const TeamPage = () => {
+const TeamPage = (props) => {
   return (
-
     <div className="team">
       <div className="container">
         <div className="section-header text-center">
           <p>Our Team</p>
           <h2>Meet Our Engineer</h2>
         </div>
-        <TeamContainer >
-          <TeamItem imgURL={require("../../../assets/img/team-1.jpg")} dataWowDelay="0.1s" person="Adam Phillips" responsibility="CEO & Founder" />
-          <TeamItem imgURL={require("../../../assets/img/team-2.jpg")} dataWowDelay="0.2s" person="Dylan Adams" responsibility="Civil Engineer" />
-          <TeamItem imgURL={require("../../../assets/img/team-3.jpg")} dataWowDelay="0.3s" person="Jhon Doe" responsibility="Interior Designer" />
-          <TeamItem imgURL={require("../../../assets/img/team-4.jpg")} dataWowDelay="0.4s" person="Josh Dunn" responsibility="Painter" />
-
-          <TeamItem imgURL={require("../../../assets/img/team-1.jpg")} dataWowDelay="0.1s" person="Adam Phillips" responsibility="CEO & Founder" />
-          <TeamItem imgURL={require("../../../assets/img/team-2.jpg")} dataWowDelay="0.2s" person="Dylan Adams" responsibility="Civil Engineer" />
-          <TeamItem imgURL={require("../../../assets/img/team-3.jpg")} dataWowDelay="0.3s" person="Jhon Doe" responsibility="Interior Designer" />
-          <TeamItem imgURL={require("../../../assets/img/team-4.jpg")} dataWowDelay="0.4s" person="Josh Dunn" responsibility="Painter" />
-        </TeamContainer>
+        {props.children}
       </div>
     </div>
   );
--- src/app/containers/Team.jsx
+++ src/app/containers/Team.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import PageHeader from '../components/PageHeader';
+import TeamContainer from '../components/team/TeamContainer';
+import TeamItem from '../components/team/TeamItem';
 import TeamPage from '../components/team/TeamPage';
 
 const Team = () => {
   return (
     <React.Fragment>
       <PageHeader name="Our Team" />
-      <TeamPage />
+      <TeamPage>
+        <TeamContainer >
+          <TeamItem imgURL={require("../../assets/img/team-1.jpg")} dataWowDelay="0.1s" person="Adam Phillips" responsibility="CEO & Founder" />
+          <TeamItem imgURL={require("../../assets/img/team-2.jpg")} dataWowDelay="0.2s" person="Dylan Adams" responsibility="Civil Engineer" />
+          <TeamItem imgURL={require("../../assets/img/team-3.jpg")} dataWowDelay="0.3s" person="Jhon Doe" responsibility="Interior Designer" />
+          <TeamItem imgURL={require("../../assets/img/team-4.jpg")} dataWowDelay="0.4s" person="Josh Dunn" responsibility="Painter" />
+
+          <TeamItem imgURL={require("../../assets/img/team-1.jpg")} dataWowDelay="0.1s" person="Adam Phillips" responsibility="CEO & Founder" />
+          <TeamItem imgURL={require("../../assets/img/team-2.jpg")} dataWowDelay="0.2s" person="Dylan Adams" responsibility="Civil Engineer" />
+          <TeamItem imgURL={require("../../assets/img/team-3.jpg")} dataWowDelay="0.3s" person="Jhon Doe" responsibility="Interior Designer" />
+          <TeamItem imgURL={require("../../assets/img/team-4.jpg")} dataWowDelay="0.4s" person="Josh Dunn" responsibility="Painter" />
+        </TeamContainer>
+      </TeamPage>
     </React.Fragment>
 
   );
